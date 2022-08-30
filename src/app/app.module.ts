3098c640602831a97b46d58e0b13002330eee557
Daniel Hopson <hdhopson88@yahoo.com>
2022-08-29 22:28:55 -0500

    Updates import paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
 import { ServicesPageComponent } from './components/services-page/services-page.component';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
 import { FaqPageComponent } from './components/faq-page/faq-page.component';
 import { BlogPageComponent } from './components/blog-page/blog-page.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
