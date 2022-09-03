d5e7d422ef34f0eb03a5ef87631ec7dd7eda3d7b
Daniel Hopson <hdhopson88@yahoo.com>
2022-09-03 16:14:01 -0500

    Creates sticky functionality for header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  navbarfixed:boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', ['$event']) onscroll() {
+    if(window.scrollY > 100) {
+      this.navbarfixed = true;
+    } else {
+      this.navbarfixed = false
+    }
   }
 
 }
